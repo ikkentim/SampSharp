a98790408a6abb88cc2ea77f46b63e8fd00654dd
Tim Potze <timpotze@gmail.com>
2014-03-16 23:02:06 +0100

    Added CheckpointType and Shopname definitions, completed documentation for all a_samp natives

---
+++ source/GameMode/Definitions/CheckpointType.cs
@@ -0,0 +1,11 @@
+﻿namespace GameMode.Definitions
+{
+    public enum CheckpointType
+    {
+        Normal = 0,
+        Finish = 1,
+        Nothing = 2,
+        Air = 3,
+        AirFinish = 4
+    }
+}
---
+++ source/GameMode/Definitions/ShopName.cs
@@ -0,0 +1,14 @@
+﻿namespace GameMode.Definitions
+{
+    public static class ShopName
+    {
+        public static string PizzaStack = "FDPIZA";
+        public static string BurgerShot = "FDBURG";
+        public static string CluckinBell = "FDCHICK";
+        public static string Ammunation1 = "AMMUN1";
+        public static string Ammunation2 = "AMMUN2";
+        public static string Ammunation3 = "AMMUN3";
+        public static string Ammunation4 = "AMMUN4";
+        public static string Ammunation5 = "AMMUN5";
+    }
+}
