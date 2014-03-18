fdfd1389014ed4d06b691534ae37b146db8450fe
Tim Potze <timpotze@gmail.com>
2014-03-18 22:37:13 +0100

    rewrote some color related stuffs and bugs

--- source/TestMode/GameMode.cs
+++ source/TestMode/GameMode.cs
@@ -14,7 +14,8 @@ namespace TestMode
 
         public override bool OnGameModeInit()
         {
-            Console.WriteLine("Player test: {0}", MyPlayer.Find(1));
+            //Console.WriteLine("Player test: {0}", MyPlayer.Find(1));
+            Console.WriteLine(new Color(255, 255, 255, 255));
             return true;
         }
     }
--- source/TestMode/World/MyPlayer.cs
+++ source/TestMode/World/MyPlayer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using GameMode.World;
 
 namespace TestMode.World
