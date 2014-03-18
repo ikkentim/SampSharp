337969c1756e8540b83a8556fbb4ea4d1f1440bf
Tim Potze <timpotze@gmail.com>
2014-03-18 22:42:12 +0100

    Added SetSpawnInfo to player class

--- source/TestMode/GameMode.cs
+++ source/TestMode/GameMode.cs
@@ -14,8 +14,6 @@ namespace TestMode
 
         public override bool OnGameModeInit()
         {
-            //Console.WriteLine("Player test: {0}", MyPlayer.Find(1));
-            Console.WriteLine(new Color(255, 255, 255, 255));
             return true;
         }
     }
