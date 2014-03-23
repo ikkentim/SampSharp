24a227c9ff3d6f5540de3f1c63025aad9a9f0473
Tim Potze <timpotze@gmail.com>
2014-03-24 00:05:17 +0100

    Made mock textdraw class, named textdraw fonts

--- source/GameMode/Definitions/TextDrawFont.cs
+++ source/GameMode/Definitions/TextDrawFont.cs
@@ -2,7 +2,34 @@
 {
     public enum TextDrawFont
     {
-        DrawSprite=4,
-        PreviewModel=5
+        /// <summary>
+        /// Font 0, also known as Diploma.
+        /// </summary>
+        Diploma = 0,
+
+        /// <summary>
+        /// Font 1, also known as Normal.
+        /// </summary>
+        Normal = 1,
+
+        /// <summary>
+        /// Font 2, also known as Slim.
+        /// </summary>
+        Slim = 2,
+
+        /// <summary>
+        /// Font 3, also known as Pricedown.
+        /// </summary>
+        Pricedown = 3,
+
+        /// <summary>
+        /// Font used to draw sprites.
+        /// </summary>
+        DrawSprite = 4,
+
+        /// <summary>
+        /// Font used to draw model previews.
+        /// </summary>
+        PreviewModel = 5
     }
 }
