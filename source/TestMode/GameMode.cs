389967f2f769db4941a0c69dea150481fe6e513e
Tim Potze <timpotze@gmail.com>
2014-05-06 22:42:22 +0200

    Added async test

--- source/TestMode/GameMode.cs
+++ source/TestMode/GameMode.cs
@@ -11,84 +11,34 @@
 // 
 // For more information, please refer to <http://unlicense.org>
 
-using System;
+using System.Collections.Generic;
 using SampSharp.GameMode;
-using SampSharp.GameMode.SAMP;
-using SampSharp.GameMode.SAMP.Commands;
 using SampSharp.GameMode.World;
+using TestMode.Tests;
 
 namespace TestMode
 {
     public class GameMode : BaseMode
     {
-        #region Hex
-        public class HexParameter : WordParameter
-        {
-            public HexParameter()
-            {
-
-            }
-
-            public HexParameter(string name) : base(name)
-            {
-
-            }
-
-            public HexParameter(string name, bool optional)
-                : base(name, optional)
-            {
-
-            }
-
-            public override bool Check(ref string command, out object output)
-            {
-                if (!base.Check(ref command, out output))
-                    return false;
-
-                string word = (string)output;
-
-                int number;
-
-
-                try
-                {
-                    number = Convert.ToInt32(word, 16);
-                }
-                catch (FormatException)
-                {
-                    output = null;
-                    return false;
-                }
-
-                output = number;
-
-                return true;
-            }
-        }
-        #endregion
-
+ 
         public override bool OnGameModeInit()
         {
-            new Command("console", new Parameter[] { new WordParameter("word"), new IntegerParameter("number", true), new TextParameter("rest", true) }, (Func<Player, string, int, string, bool>)TestCommand);
-            
-            new Command("color", new Parameter[] { new HexParameter("color"), new TextParameter("text") },
-                (Func<Player, int,string,bool>)((player, color, text) =>
-                {
-                    Color c = color;
-                    Player.SendClientMessageToAll(c, player.Name + ": " + text);
-                    return true;
-                }));
-
-
+            SetGameModeText("Test mode");
             AddPlayerClass(65, new Vector(), 0);
+
+            List<ITest> tests = new List<ITest>
+            {
+                new CommandsTest(),
+                new ASyncTest()
+            };
+
+            foreach (var test in tests)
+                test.Start(this);
+            
             return true;
         }
 
-        static bool TestCommand(Player player, string word, int number, string rest)
-        {
-            Console.WriteLine("Player: {0}, Word: {1}, Rest: {2}, Num: {3}", player, word, rest, number);
 
-            return true;
-        }
+
     }
 }
---
+++ source/TestMode/Tests/CommandsTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.World;
+
+namespace TestMode.Tests
+{
+    #region Hex
+    public class HexParameter : WordParameter
+    {
+        public HexParameter()
+        {
+
+        }
+
+        public HexParameter(string name)
+            : base(name)
+        {
+
+        }
+
+        public HexParameter(string name, bool optional)
+            : base(name, optional)
+        {
+
+        }
+
+        public override bool Check(ref string command, out object output)
+        {
+            if (!base.Check(ref command, out output))
+                return false;
+
+            string word = (string)output;
+
+            int number;
+
+
+            try
+            {
+                number = Convert.ToInt32(word, 16);
+            }
+            catch (FormatException)
+            {
+                output = null;
+                return false;
+            }
+
+            output = number;
+
+            return true;
+        }
+    }
+    #endregion
+
+    public class CommandsTest : ITest
+    {
+        public void Start(GameMode gameMode)
+        {
+            new Command("console",
+                new Parameter[]
+                {new WordParameter("word"), new IntegerParameter("number", true), new TextParameter("rest", true)},
+                (Func<Player, string, int, string, bool>) TestCommand);
+
+            new Command("color", new Parameter[] {new HexParameter("color"), new TextParameter("text")},
+                (Func<Player, int, string, bool>) ((player, color, text) =>
+                {
+                    Color c = color;
+                    Player.SendClientMessageToAll(c, player.Name + ": " + text);
+                    return true;
+                }));
+
+        }
+
+        private bool TestCommand(Player player, string word, int number, string rest)
+        {
+            Console.WriteLine("Player: {0}, Word: {1}, Rest: {2}, Num: {3}", player, word, rest, number);
+
+            return true;
+        }
+    }
+}
