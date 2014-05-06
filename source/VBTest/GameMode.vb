4524e3ea37eec146093ab13b837db14493bcb756
Tim Potze <timpotze@gmail.com>
2014-05-06 22:24:22 +0200

    Added VB test

---
+++ source/VBTest/GameMode.vb
@@ -0,0 +1,16 @@
+﻿Imports SampSharp.GameMode.SAMP
+Imports SampSharp.GameMode.Events
+Imports SampSharp.GameMode
+
+Public Class GameMode
+    Inherits BaseMode
+
+    Overrides Function OnGameModeInit() As Boolean
+        Console.WriteLine("GameModeInit from Visual basic")
+        Return True
+    End Function
+
+    Private Sub MySender_Start(ByVal sender As Object, ByVal e As PlayerEventArgs) Handles Me.PlayerConnected
+        e.Player.SendClientMessage(Color.White, "You connected to a VB gamemode!")
+    End Sub
+End Class
