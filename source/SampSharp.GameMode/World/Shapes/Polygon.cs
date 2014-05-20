e6117a304e187d70215fcce5ce7b8871c24a3f64
Tim Potze <timpotze@gmail.com>
2014-05-20 19:23:42 +0200

    Added menu class, cleanup

--- source/SampSharp.GameMode/World/Shapes/Polygon.cs
+++ source/SampSharp.GameMode/World/Shapes/Polygon.cs
@@ -1,4 +1,17 @@
-﻿namespace SampSharp.GameMode.World.Shapes
+﻿// SampSharp
+// Copyright (C) 2014 Tim Potze
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
+
+namespace SampSharp.GameMode.World.Shapes
 {
     public class Polygon : IShape
     {
@@ -6,6 +19,7 @@
         {
             Points = points;
         }
+
         public Vector[] Points { get; set; }
 
         public bool Contains(Vector point)
@@ -19,7 +33,8 @@
             {
                 if (Points[i].Y < point.Y && Points[j].Y >= point.Y || Points[j].Y < point.Y && Points[i].Y >= point.Y)
                 {
-                    if (Points[i].X + (point.Y - Points[i].Y) / (Points[j].Y - Points[i].Y) * (Points[j].X - Points[i].X) < point.X)
+                    if (Points[i].X + (point.Y - Points[i].Y)/(Points[j].Y - Points[i].Y)*(Points[j].X - Points[i].X) <
+                        point.X)
                     {
                         result = !result;
                     }
---
+++ source/TestMode/Tests/MenuTest.cs
@@ -0,0 +1,32 @@
+﻿using SampSharp.GameMode.Display;
+using SampSharp.GameMode.SAMP;
+
+namespace TestMode.Tests
+{
+    public class MenuTest : ITest
+    {
+        public void Start(GameMode gameMode)
+        {
+            gameMode.PlayerConnected += (sender, args) =>
+            {
+                Menu m = new Menu("Test menu", 0, 0, new[] {new MenuColumn(100)},
+                    new[] {new MenuRow("Active"), new MenuRow("Disabled", true), new MenuRow("Active2"),});
+
+                m.Show(args.Player);
+
+                m.Exit += (o, eventArgs) =>
+                {
+                    eventArgs.Player.SendClientMessage(Color.Red, "MENU CLOSED");
+                    m.Dispose();
+                };
+
+                m.Response += (o, eventArgs) =>
+                {
+                    eventArgs.Player.SendClientMessage(Color.Green, "SELECTED ROW " + eventArgs.Row);
+                    m.Dispose();
+                };
+            };
+        }
+
+    }
+}
