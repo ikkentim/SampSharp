9caddaf000763a03a1f3b0a7b0148335256106a8
Tim Potze <timpotze@gmail.com>
2014-05-21 17:20:35 +0200

    Added 2D vector constructors, added circle shape, made rivershell use regions

---
+++ source/SampSharp.GameMode/World/Shapes/Circle.cs
@@ -0,0 +1,23 @@
+﻿namespace SampSharp.GameMode.World.Shapes
+{
+    /// <summary>
+    /// Represents a 2D circle.
+    /// </summary>
+    public class Circle : IShape
+    {
+        public Circle(Vector position, float radius)
+        {
+            Position = position;
+            Radius = radius;
+        }
+
+        public Vector Position { get; set; }
+
+        public float Radius { get; set; }
+
+        public bool Contains(Vector point)
+        {
+            return new Vector(Position.X, Position.Y).DistanceTo(new Vector(point.X, point.Y)) < Radius;
+        }
+    }
+}
