02c979906f39288c8702e066ad1e8b1d89765f9e
Tim Potze <timpotze@gmail.com>
2014-07-13 20:54:04 +0200

    fixed issue with  symbol generation

--- src/SampSharp/MonoUtil.h
+++ src/SampSharp/MonoUtil.h
@@ -1,28 +1,25 @@
 #pragma once
 
-#include "PathUtil.h"
 #include <string>
+#include <assert.h>
 #include <mono/jit/jit.h>
-#include <sampgdk/a_samp.h>
+#include "PathUtil.h"
+
 struct MonoUtil
 {
     #ifdef _WIN32
-    static void GenerateSymbols(const char * path) {	
-	    char * p = (char *)PathUtil::GetLibDirectory()
-            .append("mono/4.5/pdb2mdb.exe").c_str();
+    static void GenerateSymbols(const char * path) {
+	    MonoAssembly *mdbconverter = mono_domain_assembly_open(mono_domain_get(), 
+            PathUtil::GetLibDirectory().append("mono/4.5/pdb2mdb.exe").c_str());
 
-	
-	    MonoAssembly *mdbconverter = mono_domain_assembly_open(
-            mono_domain_get(), p);
+        assert(mdbconverter);
 
-	    if (mdbconverter) {
-		    char *argv[2];
-		    argv[0] = p;
-		    argv[1] = (char *)path;
+		char *argv[2];
 
-		    mono_jit_exec(mono_domain_get(), mdbconverter, 2, argv);
-	    }
+		argv[0] = (char *)path;
+		argv[1] = (char *)path;
 
+		mono_jit_exec(mono_domain_get(), mdbconverter, 2, argv);
     }
     #endif
 };
