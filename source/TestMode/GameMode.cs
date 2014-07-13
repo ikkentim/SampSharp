ddcc5551adac2c257beb27bc773eb47ebe2be0ed
Tim Potze <timpotze@gmail.com>
2014-07-13 16:36:01 +0200

    Fixed and improved Native.CallNative implementation

--- source/TestMode/GameMode.cs
+++ source/TestMode/GameMode.cs
@@ -13,6 +13,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using SampSharp.GameMode;
 using SampSharp.GameMode.Definitions;
@@ -24,7 +25,7 @@ namespace TestMode
 {
     public class GameMode : BaseMode
     {
- 
+
         public override bool OnGameModeInit()
         {
             SetGameModeText("sa-mp# testmode");
@@ -43,43 +44,13 @@ namespace TestMode
                 new DialogTest(),
                 new CharsetTest(),
                 new VehicleInfoTest(),
+                new NativesTest(),
             };
 
             foreach (var test in tests)
                 test.Start(this);
 
-            
-            Console.WriteLine("CALLING GetNetworkStats");
-            string str = "";
-            Native.CallNative("GetNetworkStats", __arglist(ref str, 400));
-            Console.WriteLine(str);
-
-            Console.WriteLine("CALLING SetGameModeText");
-            Native.CallNative("SetGameModeText", __arglist("SetGameModeText"));
-
-            Console.WriteLine("CALLING SetGravity");
-            Native.CallNative("SetGravity", __arglist(0.42f));
-            
-            Console.WriteLine("CALLING GetVehicleModelInfo (Doesn't work yet)");
-            float x = 1;
-            float y = 2;
-            float z = 3;
-            Native.CallNative("GetVehicleModelInfo", __arglist(400, (int)VehicleModelInfoType.Size, ref x, ref y, ref z));
-            Console.WriteLine("{0} {1} {2}", x, y, z);
-            
-            Console.WriteLine("CALLING GetWeaponName");
-            string weapon = "empty";
-
-            Native.CallNative("GetWeaponName", __arglist((int)Weapon.MP5, ref weapon, 32));
-            Console.WriteLine("Weapon: {0}", weapon);
             return true;
-        }
-
-        public override bool OnRconCommand(string command)
-        {
-            Console.WriteLine("[DEBUG-RCON] " + command);
-
-            return base.OnRconCommand(command);
         }
     }
 }
