1cfa34b9f1dfce78fc45aa87e4c009047667f6ff
Tim Potze <timpotze@gmail.com>
2014-07-16 21:19:44 +0200

    Added basic benchmark test(don't believe it properly works though, seeing the results)

---
+++ src/BenchmarkMode/Tests/NativeIsPlayerConnected.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SampSharp.GameMode.Natives;
+
+namespace BenchmarkMode.Tests
+{
+    public class NativeIsPlayerConnected : ITest
+    {
+        public void Run(GameMode gameMode)
+        {
+            bool v = Native.IsPlayerConnected(0);
+        }
+    }
+}
