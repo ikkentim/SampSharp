0cdc2280af857c63f91397919c117f25818429ac
Tim Potze <timpotze@gmail.com>
2014-07-23 10:13:29 +0200

    [Streamer] Added events to main object

---
+++ src/SampSharp.Streamer/Events/DynamicObjectEventArgs.cs
@@ -0,0 +1,34 @@
+﻿// SampSharp
+// Copyright (C) 2014 Tim Potze
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
+
+using SampSharp.GameMode.Events;
+using SampSharp.GameMode.World;
+using SampSharp.Streamer.World;
+
+namespace SampSharp.Streamer.Events
+{
+    public class DynamicObjectEventArgs : GameModeEventArgs
+    {
+        public DynamicObjectEventArgs(int objectid)
+        {
+            ObjectId = objectid;
+        }
+
+        public int ObjectId { get; private set; }
+
+        public DynamicObject DynamicObject
+        {
+            get { return ObjectId == GlobalObject.InvalidId ? null : DynamicObject.FindOrCreate(ObjectId); }
+        }
+    }
+}
