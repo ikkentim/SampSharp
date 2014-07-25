369a137d7b1729bef9ca5c1d3fdfe17cb764dd7e
Tim Potze <timpotze@gmail.com>
2014-07-25 13:20:36 +0200

    Fixed sync request not being disposed after running, added docs on sync

--- src/SampSharp.GameMode/Tools/Sync.cs
+++ src/SampSharp.GameMode/Tools/Sync.cs
@@ -1,16 +1,41 @@
-﻿using System;
+﻿// SampSharp
+// Copyright (C) 2014 Tim Potze
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
+
+using System;
 using System.Threading;
 using SampSharp.GameMode.Controllers;
 using SampSharp.GameMode.Pools;
 
 namespace SampSharp.GameMode.Tools
 {
+    /// <summary>
+    ///     Contains methods to run an action on the main VM thread from a different thread.
+    /// </summary>
     public static class Sync
     {
+        /// <summary>
+        ///     Run a function on the main VM thread.
+        /// </summary>
+        /// <param name="action">The action the run</param>
         public static void Run(Action action)
         {
-            var task = new SyncTask { Action = action };
+            if (SyncController.IsMainThread)
+            {
+                action();
+                return;
+            }
 
+            var task = new SyncTask {Action = action};
             SyncController.Start();
 
             while (!task.Done)
@@ -18,22 +43,33 @@ namespace SampSharp.GameMode.Tools
                 Thread.Sleep(1);
             }
         }
+
+        /// <summary>
+        ///     Run a function on the main VM thread.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the return value of the method that the action encapsulates.</typeparam>
+        /// <param name="action">The action to run.</param>
+        /// <returns>The return value of the method that the action encapsulates.</returns>
         public static TResult Run<TResult>(Func<TResult> action)
         {
+            if (SyncController.IsMainThread)
+            {
+                return action();
+            }
+
             TResult result = default(TResult);
 
-            Run(() =>
-            {
-                result = action();
-            });
-            
+            Run(() => { result = action(); });
+
             return result;
         }
 
-        public class SyncTask : Pool<SyncTask>
+        internal sealed class SyncTask : Pool<SyncTask>
         {
             public Action Action { get; set; }
+
             public bool Done { get; private set; }
+
             public void Run()
             {
                 Action();
