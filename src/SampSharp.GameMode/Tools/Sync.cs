a881ee76c1f9517f13e202914dbbc1ad7ad22144
Tim Potze <timpotze@gmail.com>
2014-07-25 13:08:10 +0200

    Added way of resyncing to main thread from async thread, made Pool threadsafe

---
+++ src/SampSharp.GameMode/Tools/Sync.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using SampSharp.GameMode.Controllers;
+using SampSharp.GameMode.Pools;
+
+namespace SampSharp.GameMode.Tools
+{
+    public static class Sync
+    {
+        public static void Run(Action action)
+        {
+            var task = new SyncTask { Action = action };
+
+            SyncController.Start();
+
+            while (!task.Done)
+            {
+                Thread.Sleep(1);
+            }
+        }
+        public static TResult Run<TResult>(Func<TResult> action)
+        {
+            TResult result = default(TResult);
+
+            Run(() =>
+            {
+                result = action();
+            });
+            
+            return result;
+        }
+
+        public class SyncTask : Pool<SyncTask>
+        {
+            public Action Action { get; set; }
+            public bool Done { get; private set; }
+            public void Run()
+            {
+                Action();
+                Done = true;
+            }
+        }
+    }
+}
