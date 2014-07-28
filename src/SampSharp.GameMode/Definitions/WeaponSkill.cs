7be7549103d775a07b7ec13b9af3a0c13cd41bab
Tim Potze <timpotze@gmail.com>
2014-07-28 18:08:24 +0200

    Updated some documentation

--- src/SampSharp.GameMode/Definitions/WeaponSkill.cs
+++ src/SampSharp.GameMode/Definitions/WeaponSkill.cs
@@ -14,20 +14,53 @@
 namespace SampSharp.GameMode.Definitions
 {
     /// <summary>
-    ///     Contains all weapon skills.
+    ///     Contains all weapon skills types.
     /// </summary>
     public enum WeaponSkill
     {
+        /// <summary>
+        /// Pistol skills.
+        /// </summary>
         Pistol = 0,
+        /// <summary>
+        /// Silenced pistol skills.
+        /// </summary>
         PistolSilenced = 1,
+        /// <summary>
+        /// Desert eagle skills.
+        /// </summary>
         DesertEagle = 2,
+        /// <summary>
+        /// Shotgun skills.
+        /// </summary>
         Shotgun = 3,
+        /// <summary>
+        /// Sawn-off shotgun skills.
+        /// </summary>
         SawnoffShotgun = 4,
+        /// <summary>
+        /// Combat shotgun skills.
+        /// </summary>
         Spas12Shotgun = 5,
+        /// <summary>
+        /// Micro uzi skills.
+        /// </summary>
         MicroUzi = 6,
+        /// <summary>
+        /// MP5 skills.
+        /// </summary>
         MP5 = 7,
+        /// <summary>
+        /// AK47 skills.
+        /// </summary>
         AK47 = 8,
+        /// <summary>
+        /// M4 skills.
+        /// </summary>
         M4 = 9,
+        /// <summary>
+        /// Sniper rifle skills.
+        /// </summary>
         SniperRifle = 10
     }
 }
