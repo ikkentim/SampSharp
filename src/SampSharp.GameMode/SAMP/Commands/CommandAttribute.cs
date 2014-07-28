271f08650803b90b695f27e783fad2e8e812e552
Tim Potze <timpotze@gmail.com>
2014-07-28 18:08:24 +0200

    Updated some documentation

--- src/SampSharp.GameMode/SAMP/Commands/CommandAttribute.cs
+++ src/SampSharp.GameMode/SAMP/Commands/CommandAttribute.cs
@@ -15,23 +15,48 @@ using System;
 
 namespace SampSharp.GameMode.SAMP.Commands
 {
+    /// <summary>
+    /// Indicates that a method is a player-comammand and specifies information about the command.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class CommandAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name of the command.</param>
         public CommandAttribute(string name)
         {
             Name = name;
             IgnoreCase = true;
         }
 
+        /// <summary>
+        /// Gets or sets the name of this Command.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether this Command is case-sensitive.
+        /// </summary>
         public bool IgnoreCase { get; set; }
 
+        /// <summary>
+        /// Gets or sets an alias of this Command.
+        /// </summary>
         public string Alias { get; set; }
 
+        /// <summary>
+        /// Gets or sets a shortcut of this Command.
+        /// </summary>
+        /// <remarks>
+        /// A shortcut is the same as an alias, but without the commandgroup in front of it.
+        /// </remarks>
         public string Shortcut { get; set; }
 
+        /// <summary>
+        /// Gets or sets the method run to check whether a player has the permissions the run the command.
+        /// </summary>
         public string PermissionCheckMethod { get; set; }
     }
 }
