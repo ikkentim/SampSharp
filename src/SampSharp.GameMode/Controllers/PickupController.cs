7af0613ec1346097adf34bba22a51cba602817a2
Marco Acierno <marcoaciernoemail@gmail.com>
2014-08-11 01:40:40 +0200

    pickup support implemented

---
+++ src/SampSharp.GameMode/Controllers/PickupController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampSharp.GameMode.Display;
+using SampSharp.GameMode.World;
+
+namespace SampSharp.GameMode.Controllers
+{
+    public class PickupController : IEventListener, ITypeProvider
+    {
+        /// <summary>
+        ///     Registers the events this TextDrawController wants to listen to.
+        /// </summary>
+        /// <param name="gameMode">The running GameMode.</param>
+        public void RegisterEvents(BaseMode gameMode)
+        {
+            gameMode.PlayerPickUpPickup += (sender, args) =>
+            {
+                var obj = Pickup.Find(args.PickupId);
+
+                if (obj != null)
+                {
+                    obj.OnPickup(args);
+                }
+            };
+        }
+
+        /// <summary>
+        ///     Registers types this PickupController requires the system to use.
+        /// </summary>
+        public void RegisterTypes()
+        {
+            Pickup.Register<Pickup>();
+        }
+    }
+}
