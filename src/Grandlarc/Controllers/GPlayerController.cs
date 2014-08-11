5af1a82abf5d0f8b7a7519276c12b15e167d916c
Marco Acierno <marcoaciernoemail@gmail.com>
2014-08-11 22:36:03 +0200

    Grandlarc gamemode
    It's the grandlarc gamemode ported to C#. With additional commands /kill
    and /changecity to change the city in use.

---
+++ src/Grandlarc/Controllers/GPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampSharp.GameMode.Controllers;
+
+namespace Grandlarc.Controllers
+{
+    public class GPlayerController : PlayerController
+    {
+        public override void RegisterTypes()
+        {
+            GPlayer.Register<GPlayer>();
+        }
+    }
+}
