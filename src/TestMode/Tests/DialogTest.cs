9698b34d019221ce394f305411032ab6657c01e1
Tim Potze <timpotze@gmail.com>
2015-06-25 19:59:03 +0200

    Added async dialog test method

--- src/TestMode/Tests/DialogTest.cs
+++ src/TestMode/Tests/DialogTest.cs
@@ -13,9 +13,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Linq;
+using SampSharp.GameMode;
 using SampSharp.GameMode.Definitions;
 using SampSharp.GameMode.Display;
+using SampSharp.GameMode.Natives;
+using SampSharp.GameMode.SAMP;
 using SampSharp.GameMode.SAMP.Commands;
 using SampSharp.GameMode.Tools;
 using SampSharp.GameMode.World;
@@ -64,7 +68,27 @@ namespace TestMode.Tests
             var dialog = new Dialog(DialogStyle.Input, "Hello", "Insert something", "Confirm", "NO");
             var response = await dialog.ShowAsync(player);
 
+            Console.WriteLine(Sync.IsRequired);
             Sync.Run(() => { player.SendClientMessage("Response: " + response.InputText); });
+        }
+
+        [Command("dialogasynclogintest")]
+        public static async void DialogASyncCommandLogintest(GtaPlayer player)
+        {
+            var dialog = new InputDialog("Hello", "Login please!", true, "Login", "Cancel");
+
+            var timerToShowThingsWork = new Timer(1000, true);
+            timerToShowThingsWork.Tick += (sender, args) =>
+            {
+                player.SendClientMessage("Things still work in the background!");
+            };
+
+            var response = await dialog.ShowAsync(player);
+
+            player.SendClientMessage("Your input was {1}... Do we require a sync? {0}. So thats awesome! Lets move you up a lill. ", Sync.IsRequired, response.InputText);
+            player.Position += new Vector3(0,1,0);
+
+            timerToShowThingsWork.Dispose();
         }
     }
 }
