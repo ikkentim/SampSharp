acfaa9e2a4e467b0e429a90a9d0eaa35e1364dc5
Tim Potze <timpotze@gmail.com>
2015-12-17 00:22:31 +0100

    Increase version to avoid confusion in documentation

--- src/SampSharp.GameMode/Properties/AssemblyInfo.cs
+++ src/SampSharp.GameMode/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
