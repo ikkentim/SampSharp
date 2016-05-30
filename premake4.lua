a229f074ee5492fa797d31aeecde7ea98f2e7e51
Tim Potze <timpotze@gmail.com>
2016-05-30 21:51:10 +0200

    Updated CI files

--- premake4.lua
+++ premake4.lua
@@ -1,30 +1,40 @@
 -- A solution contains projects, and defines the available configurations
 solution "SampSharp"
-   configurations { "Debug", "Release" }
+    configurations { "debug", "release" }
+    platforms { "x32", "x64" }
 
-   -- A project defines one build target
-   project "SampSharp"
-      targetname "SampSharp"
-      kind "SharedLib"
+    -- A project defines one build target
+    project "SampSharp"
+        targetname "SampSharp"
+        kind "SharedLib"
 
-      language "C++"
-      platforms { "x32" }
-      links { "mono-2.0", "rt" }
-      
-      includedirs { "src/SampSharp/includes", "src/SampSharp/includes/sdk", "src/SampSharp/includes/sdk/amx" }
-      buildoptions { "-fvisibility=hidden", "-fvisibility-inlines-hidden", "-m32", "-std=c++11" }
-      
-      files { "src/SampSharp**.cpp", "src/SampSharp/includes/sampgdk/sampgdk.c" }
+        language "C++"
+        links { "mono-2.0", "rt" }
 
-      configuration "Debug"
-         objdir "obj/Debug"
-         targetdir "env/plugins"
-         defines { "DEBUG", "LINUX", "_GNU_SOURCE", "SAMPGDK_AMALGAMATION" }
-         flags { "Symbols" }
+        includedirs {
+            "src/SampSharp/includes",
+            "src/SampSharp/includes/sdk",
+            "src/SampSharp/includes/sdk/amx"
+        }
+        buildoptions {
+            "-fvisibility=hidden",
+            "-fvisibility-inlines-hidden",
+            "-std=c++11"
+        }
 
-      configuration "Release"
-         objdir "obj/Release"
-         targetdir "bin"
-         defines { "NDEBUG", "LINUX", "_GNU_SOURCE", "SAMPGDK_AMALGAMATION" }
-         flags { "Optimize" }
+        files { "src/SampSharp**.cpp", "src/SampSharp/includes/sampgdk/sampgdk.c" }
 
+        configuration "x64"
+            defines { "__i386__" }
+
+        configuration "Debug"
+            objdir "obj/Debug"
+            targetdir "env/plugins"
+            defines { "DEBUG", "LINUX", "_GNU_SOURCE", "SAMPGDK_AMALGAMATION" }
+            flags { "Symbols" }
+
+        configuration "Release"
+            objdir "obj/Release"
+            targetdir "bin"
+            defines { "NDEBUG", "LINUX", "_GNU_SOURCE", "SAMPGDK_AMALGAMATION" }
+            flags { "Optimize" }
