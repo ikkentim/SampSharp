a195b3fd850105ba606b62ccb352ab6ff5eca510
Tim Potze <timpotze@gmail.com>
2016-10-13 10:59:31 +0200

    Update MonoRuntime.cpp

--- src/SampSharp/MonoRuntime.cpp
+++ src/SampSharp/MonoRuntime.cpp
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 #include "MonoRuntime.h"
-#include <stdio.h>
+#include <string.h>
 #include <mono/jit/jit.h>
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/mono-debug.h>
