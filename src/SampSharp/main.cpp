58bd91f83cbb4102663fd6f91d845aa222aebf21
Tim Potze <timpotze@gmail.com>
2017-11-23 21:00:11 +0100

    Make debugger pausing detection opt-in instead of opt-out

--- src/SampSharp/main.cpp
+++ src/SampSharp/main.cpp
@@ -132,12 +132,12 @@ void start_server() {
     }
 #endif
 
-    std::string nodebug_str;
-    plg->config()->GetOptionAsString("com_nodebug", type);
-    bool nodebug = nodebug_str == "1" || nodebug_str == "true";
+    std::string debug_str;
+    plg->config()->GetOptionAsString("com_debug", type);
+    bool debug = debug_str == "1" || debug_str == "true";
 
     if (com) {
-        svr = new server(plg, com, !nodebug);
+        svr = new server(plg, com, debug);
         svr->start();
     }
 }
