e298b16460d899446b764861b550fe4bae33cbc5
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-04-19 12:04:13 +0200

    refactor: respect naming convention for fields

--- src/SampSharp.GameMode/SAMP/Commands/ParameterTypes/IntegerType.cs
+++ src/SampSharp.GameMode/SAMP/Commands/ParameterTypes/IntegerType.cs
@@ -21,8 +21,8 @@ namespace SampSharp.GameMode.SAMP.Commands.ParameterTypes;
 /// <summary>Represents an integer command parameter.</summary>
 public class IntegerType : ICommandParameterType
 {
-    private static readonly char[] Base10Characters = "1234567890,.-".ToCharArray();
-    private static readonly char[] Base16Characters = "1234567890abcdef".ToCharArray();
+    private static readonly char[] _base10Characters = "1234567890,.-".ToCharArray();
+    private static readonly char[] _base16Characters = "1234567890abcdef".ToCharArray();
 
     /// <summary>Gets the value for the occurrence of this parameter type at the start of the commandText. The processed text will be removed from the commandText.</summary>
     /// <param name="commandText">The command text.</param>
@@ -43,7 +43,7 @@ public class IntegerType : ICommandParameterType
 
 
         // Regular base 10 numbers (eg. 14143)
-        if (word.All(Base10Characters.Contains) && int.TryParse(word, out var number))
+        if (word.All(_base10Characters.Contains) && int.TryParse(word, out var number))
         {
             commandText = commandText.Substring(word.Length)
                 .TrimStart(' ');
@@ -62,7 +62,7 @@ public class IntegerType : ICommandParameterType
             base16Word = word.Substring(0, word.Length - 1);
 
         if (base16Word != null && base16Word.ToLower()
-                .All(Base16Characters.Contains) && int.TryParse(base16Word, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out number))
+                .All(_base16Characters.Contains) && int.TryParse(base16Word, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out number))
         {
             commandText = commandText.Substring(word.Length)
                 .TrimStart(' ');
