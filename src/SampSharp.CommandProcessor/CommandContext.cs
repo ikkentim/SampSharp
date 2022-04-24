620fb3d08e031e53c01b405bccd248a9b6fb5cca
Tim Potze <timpotze@gmail.com>
2022-04-24 20:56:25 +0200

    removed some notes

--- src/SampSharp.CommandProcessor/CommandContext.cs
+++ src/SampSharp.CommandProcessor/CommandContext.cs
@@ -1,11 +1,4 @@
 ﻿namespace SampSharp.CommandProcessor
 {
-    // requirements
-    // generic
-    // overloads
-    // usage messages
-    // help commands
-    // localization
-
     public record CommandContext(ICommandProcessor CommandProcessor, CommandProcessorOptions Options, object? UserContext);
 }
