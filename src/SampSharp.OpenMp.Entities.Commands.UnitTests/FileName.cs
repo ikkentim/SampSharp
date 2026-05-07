eb2dc9b65ce58225955a30e83d165a8688babca4
Tim Potze <timpotze@gmail.com>
2026-05-08 00:10:29 +0200

    setup unit test project

---
+++ src/SampSharp.OpenMp.Entities.Commands.UnitTests/FileName.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace SampSharp.OpenMp.Entities.Commands.UnitTests
+{
+    public class FileName
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
