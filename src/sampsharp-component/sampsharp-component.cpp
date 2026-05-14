8526ad5ae927a2ee88f0f13403b7efbd31deeed4
Tim Potze <timpotze@gmail.com>
2026-05-15 00:38:20 +0200

    Update branding to 1.0.0 prerelease 1 (#563)

--- src/sampsharp-component/sampsharp-component.cpp
+++ src/sampsharp-component/sampsharp-component.cpp
@@ -25,7 +25,7 @@ StringView SampSharpComponent::componentName() const
 
 SemanticVersion SampSharpComponent::componentVersion() const
 {
-    return {VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_BUILD};
+    return { VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, 0 };
 }
 
 void SampSharpComponent::onLoad(ICore* c)
@@ -37,6 +37,12 @@ void SampSharpComponent::onLoad(ICore* c)
     {
         sampsharp::crash::install(c);
     }
+
+#if VERSION_PRERELEASE > 0
+    c->logLn(LogLevel::Warning, "SampSharp - You are running a prerelease version of SampSharp. Expect instability and report any issues you encounter to the developers.");
+    c->logLn(LogLevel::Warning, "SampSharp - Version: %d.%d.%d prerelease %d", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_PRERELEASE);
+    c->logLn(LogLevel::Warning, "SampSharp - Report issues here: https://github.com/ikkentim/SampSharp/issues");
+#endif
 }
 
 void SampSharpComponent::provideConfiguration(ILogger& logger, IEarlyConfig& config, const bool defaults)
