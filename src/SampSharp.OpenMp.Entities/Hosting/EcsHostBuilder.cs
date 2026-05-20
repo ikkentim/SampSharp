673fe30d1c7fab7ebba30f19dbfb3b98caf708b8
Tim Potze <timpotze@gmail.com>
2026-05-20 23:53:04 +0200

    Adjust logger namespaces (#584)

--- src/SampSharp.OpenMp.Entities/Hosting/EcsHostBuilder.cs
+++ src/SampSharp.OpenMp.Entities/Hosting/EcsHostBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using SampSharp.Entities.Logging;
 using SampSharp.Entities.SAMP;
 using SampSharp.OpenMp.Core;
 
