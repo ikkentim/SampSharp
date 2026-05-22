617e87ab1f5f49f7c40d98f26da1969ac3c7c10b
Tim Potze <timpotze@gmail.com>
2026-05-22 21:10:52 +0200

    Get environment from sampsharp.environment var, DOTNET_ENVIRONMENT, ASPNETCORE_ENVIRONMENT

---
+++ src/SampSharp.OpenMp.Entities/Hosting/ConfigurationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SampSharp.Entities;
+
+internal class ConfigurationFactory
+{
+    public static IConfiguration Create(SampSharpEnvironment environment, Action<IConfigurationBuilder> configure)
+    {
+        var basePath = Path.GetDirectoryName(environment.EntryAssembly.Location) ?? ".";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .Add(new OpenMpConfigProvider(environment))
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+        configure(builder);
+
+        return builder.Build();
+    }
+
+}
